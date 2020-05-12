282a8424dff6fa973d334fbfa34f8fa5a2f78dca
Marina Kuznetsova <marinaburykina@gmail.com>
2020-05-12 19:56:31 +0300

    instagram-links

---
+++ components/ui/InstaItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <img :src="url" class="insta__image">
+</template>
+
+<script>
+  export default {
+    props: ['url'],
+  }
+</script>
+
+<style scoped>
+
+
+
+.insta__image {
+   /* Temp */
+  width: 195px;
+  height: 100%;
+  object-fit: cover;
+}
+
+
+</style>
