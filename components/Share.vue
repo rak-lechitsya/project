2237279c23a06e455fe9f75b1034af29a5e28ad2
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-05-18 21:26:36 +0500

    adaptive Share

--- components/Share.vue
+++ components/Share.vue
@@ -99,7 +99,7 @@ export default {
 }
 
 .share__text-paragraph {
-  min-height: 88px;
+  min-height: 90px;
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 22px;
@@ -120,5 +120,190 @@ export default {
 
 .share__text-span:hover {
   color: black;
+}
+
+@media (min-width : 320px) and (max-width : 499px) {
+  .share {
+    padding: 50px 10px;
+  }
+
+  .share__content {
+    max-width: 295px;
+  }
+
+  .share__title {
+    margin: 0 auto 16px;
+    font-size: 21px;
+    line-height: 18px;
+  }
+
+  .share__container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .share__text {
+    display: block;
+  }
+
+  .share__subtitle {
+    margin-bottom: 40px;
+    font-size: 13px;
+    line-height: 16px;
+    max-width: 295px;
+  }
+
+  .share__text-span {
+    margin: 0 16px 16px 0;
+    font-size: 13px;
+    line-height: 19px;
+    text-align: center;
+  }
+
+  .share__span-container {
+    display: flex;
+  }
+
+  .share__text-paragraph {
+    font-size: 13px;
+    line-height: 16px;
+    min-height: 98px;
+  }
+
+  .share__text-span_is_active {
+    border-bottom: 2px solid #613A93;
+  }
+
+  .share__text-button {
+    width: 290px;
+    height: 40px;
+    margin-top: 30px;
+    font-size: 13px;
+    line-height: 16px;
+  }
+}
+
+@media (min-width : 500px) and (max-width : 979px) {
+  .share {
+    padding: 80px 50px;
+  }
+
+  .share__content {
+    max-width: 390px;
+  }
+
+  .share__title {
+    text-align: center;
+    margin: 0 auto 26px;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  .share__container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .share__text {
+    display: block;
+  }
+
+  .share__subtitle {
+    margin-bottom: 80px;
+    font-size: 13px;
+    line-height: 16px;
+    max-width: 380px;
+  }
+
+  .share__text-span {
+    margin: 0 30px 24px 0;
+    font-size: 15px;
+    line-height: 19px;
+    text-align: center;
+  }
+
+  .share__span-container {
+    display: flex;
+  }
+
+  .share__text-paragraph {
+    font-size: 15px;
+    line-height: 19px;
+    min-height: 97px;
+  }
+
+  .share__text-span_is_active {
+    border-bottom: 2px solid #613A93;
+  }
+
+  .share__text-button {
+    width: 230px;
+    height: 48px;
+    margin-top: 59px;
+    font-size: 15px;
+    line-height: 18px;
+  }
+}
+
+
+@media (min-width : 980px) and (max-width : 1199px) {
+  .share {
+    padding: 80px 50px;
+  }
+
+  .share__title {
+    margin: 0 0 30px;
+    font-size: 24px;
+    line-height: 28px;
+    max-width: 288px;
+  }
+
+  .share__subtitle {
+    font-size: 13px;
+    line-height: 16px;
+    max-width: 260px;
+  }
+
+  .share__text-span {
+    margin: 0 30px 8px 0;
+    font-size: 15px;
+    line-height: 19px;
+  }
+
+  .share__text-container {
+    max-width: 457px;
+  }
+
+  .share__text-paragraph {
+    font-size: 15px;
+    line-height: 19px;
+    min-height: 78px;
+  }
+
+  .share__text-button {
+    width: 230px;
+    height: 46px;
+    font-size: 15px;
+    line-height: 18px;
+  }
+}
+
+@media (min-width : 1200px) and (max-width : 1280px) {
+  .share {
+    padding: 80px 50px 90px;
+  }
+
+  .share__title {
+    margin: 0 0 30px;
+  }
+
+  .share__text-container {
+    max-width: 580px;
+  }
+
+  .share__text-button {
+    width: 230px;
+    height: 48px;
+  }
 }
 </style>
